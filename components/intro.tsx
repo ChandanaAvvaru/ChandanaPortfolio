eab492248d977b0a10b6af5f01b498f7cc868d21
saichandana2410 <saichandanaavvaru@gmail.com>
2024-06-07 18:49:15 -0400

    build errors

--- components/intro.tsx
+++ components/intro.tsx
@@ -49,8 +49,8 @@ export default function Intro() {
         <motion.h1 className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
         initial={{opacity: 0, y:100}}
         animate={{opacity:1,y:0}}>
-        <span className="font-bold">Hello, I'm Chandana.</span> I'm a{" "}
-        <span className="font-bold">Master's Student</span> at{" "}
+        <span className="font-bold">Hello, I am Chandana.</span> I am a{" "}
+        <span className="font-bold">Masters Student</span> at{" "}
         <span className="font-bold">Pennsylvania State University.</span> I also have an experience
         of 3 years at {" "}<span className="italic">Optum - United Health Group</span>. I am looking for{" "}
         <span className="underline">summer 2024 internships</span>.
